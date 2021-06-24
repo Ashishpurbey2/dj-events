0c12cd32dd93f36129a07bf155e440b62eb81353
Ashish Kumar <ashishpurbey221806@gmail.com>
2021-06-24 15:24:18 +0530

    first commit

---
+++ dj-events-frontend/pages/events/add.js
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout'
+
+export default function addEventPage() {
+  return (
+    <Layout title='Add New Events'>
+      <h1>Add Event</h1>
+    </Layout>
+  )
+}
